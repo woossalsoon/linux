7cd61f7e658f63cc6b4970c04c70ea1fa13d99a8
woossalsoon <wjdgustkdlqj@naver.com>
2021-10-14 15:40:48 +0900

    .

--- MT.sh
+++ MT.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
-if [ $1 -lt 1 ];then
-	echo "Enter number only 1~9"
-fi
-if [ $1 -gt 9 ];then
-	echo "Enter number only 1~9"
-fi
-if [ $2 -lt 1 ];then
-	echo "Enter number only 1~9"
-fi
-if [ $2 -gt 9 ];then
-	echo "Enter number only 1~9"
-fi
 for row in $(seq $1)
 do
+	if [ $1 -lt 1 ];then
+		echo "Enter 1~9"
+		break
+	fi
+	if [ $1 -gt 9 ];then
+		echo "Enter 1~9"
+		break
+	fi
+	if [ $2 -lt 1 ];then
+		echo "Enter 1~9"
+		break
+	fi
+	if [ $2 -gt 9 ];then
+		echo "Enter 1~9"
+		break
+	fi
 	for col in $(seq $2)
 	do
 		printf "$row * $col = $(($row * $col)) 	"
